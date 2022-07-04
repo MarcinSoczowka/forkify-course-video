9da54e48ff510dfe3a6d70690522d7a82646558f
MarcinSoczowka <martin.soczowka@gmail.com>
2022-07-04 18:27:27 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7bc4cf1e-687d-45d0-9436-cb94418360f4';
+export const MODAL_CLOSE_SEC = 2.5;
